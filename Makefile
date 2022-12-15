e4ae9ead2c530b7e25fcc2b5731572658317d975
Liran BG <liranbg@users.noreply.github.com>
2022-12-15 20:22:56 +0200

    CI - Add lint (#43)

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Copyright 2022 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set the default shell to bash instead of sh
+SHELL := bash
+
+# Set the default target to help
+.DEFAULT_GOAL := help
+
+.PHONY: help
+help: ## Display available commands
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: tests
+tests: ## Run tests
+	@./tests/run.sh
---
+++ tests/run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Copyright 2022 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+dirname=$(dirname $0)
+
+# Install chart dependencies
+echo "Installing chart dependencies"
+cd "$dirname"/../charts/mlrun-ce
+helm dependency update
+cd -
+
+# Install MLRun CE
+echo "Installing MLRun CE"
+helm install mlrun \
+    --create-namespace \
+    --namespace mlrun \
+    --devel \
+    --wait \
+    --debug \
+    --timeout 960s \
+    ./charts/mlrun-ce
